f3ec341371b8d33ba42195572d72d41856a5ea1d
milanraja123 <milanraja5555@gmail.com>
2023-06-07 13:58:31 +0530

    last commit

--- src/context/Filter_context.js
+++ src/context/Filter_context.js
@@ -3,14 +3,11 @@ import { useProductContext } from "./Productcontext";
 
 const FilterContext = createContext();
 
-const initialState = () => {
-  
-}
+const initialState = () => {};
 
 export const FilterContextProvider = ({ children }) => {
-
-  const {products} = useProductContext();
-  const [state,dispatch] = useReducer(reducer,initialState);
+  const { products } = useProductContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <FilterContext.Provider value={{ ...state }}>
